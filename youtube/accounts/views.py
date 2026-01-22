0dc96e3432878256f93c0c088fa9825f5ab599ba
Dimitar Pashev <dim.pashev@gmail.com>
2026-01-22 13:13:01 +0200

    add forms and views

--- youtube/accounts/views.py
+++ youtube/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "accounts/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return redirect("/")
